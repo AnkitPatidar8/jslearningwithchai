6210f483eb519a57d9f34d5e741447cb5e65010b
AnkitPatidar8 <ankitpatidar7133@gmail.com>
2024-09-25 09:42:48 +0000

    new

---
+++ 02_basic/new.js
@@ -0,0 +1,19 @@
+console.log("ankit ")
+
+const accountId = 123456 
+
+// accountId = 14  // ye allowed nhi h 
+  
+ console.log(accountId);
+
+ //  let 
+
+  let accountemail = "ankitpatidar@gmail.com"
+   
+    accountemail = "abc@mail"
+
+    console.log(accountemail);
+ var accountPassword = "258"
+ let accountCity = "bangulure" 
+
+ console.table([accountId,accountemail,accountPassword,accountCity]);
